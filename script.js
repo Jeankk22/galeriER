b64a90c117f52668bec32194e79ff6472d115f38
Jeankk22 <118958980+Jeankk22@users.noreply.github.com>
2023-11-20 11:39:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const gallery = document.getElementById("imageGallery");
+
+const directoryPath = "uploads/";
+
+// An array to hold image file names dynamically
+const imageFiles = [];
+
+// Function to load images from the "uploads" directory
+function loadImages() {
+    fetch(directoryPath)
+        .then((response) => response.text())
+        .then((data) => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(data, "text/html");
+            const links = Array.from(doc.querySelectorAll('a'));
+            
+            links.forEach((link) => {
+                const fileName = link.href.split("/").pop();
+                if (/\.(jpg|jpeg)$/i.test(fileName)) {
+                    imageFiles.push(fileName);
+                }
+            });
+            
+            displayImages();
+        })
+        .catch((error) => {
+            console.error("Error loading images:", error);
+        });
+}
+
+// Function to display images
+function displayImages() {
+    imageFiles.forEach((fileName) => {
+        const imageDiv = document.createElement("div");
+        imageDiv.className = "image";
+
+        const image = document.createElement("img");
+        image.src = directoryPath + fileName;
+        image.alt = fileName;
+
+        imageDiv.appendChild(image);
+        gallery.appendChild(imageDiv);
+    });
+}
+
+loadImages();
